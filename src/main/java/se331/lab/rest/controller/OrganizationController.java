2f18f9eb1fa3c530c8f329b9f3c312768d1c613b
Kitsada <kitsada_t@cmu.ac.th>
2021-09-30 21:00:42 +0700

    3.2

--- src/main/java/se331/lab/rest/controller/OrganizationController.java
+++ src/main/java/se331/lab/rest/controller/OrganizationController.java
@@ -2,6 +2,7 @@ package se331.lab.rest.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.PageRequest;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -24,6 +25,16 @@ public class OrganizationController {
         return ResponseEntity.ok(LabMapper.INSTANCE.getOrganizerDTO(organizerService.getAllOrganizer()));
     }
 
+    @GetMapping("organizers/{id}")
+    public ResponseEntity<?> getOrganizer(@PathVariable("id") Long id) {
+
+        Organizer output = organizerService.getOrganizer(id);
+        if (output != null) {
+            return ResponseEntity.ok(LabMapper.INSTANCE.getOrganizerDTO(output));
+        } else {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "The given id is not found");
+        }
+    }
     @PostMapping("/organizers")
     public ResponseEntity<?> addEvent(@RequestBody Organizer organizer) {
         Organizer output = organizerService.save(organizer);
